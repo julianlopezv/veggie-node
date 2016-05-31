45f5451a449a798b6df4e6cdc27d4f018314e153
Julián Andrés López Valencia <julian.lopezv@udea.edu.co>
2016-05-31 17:14:37 -0500

    Se creo servicio para autenticación.

---
+++ README.md
@@ -0,0 +1,44 @@
+# Veggie Check 
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-url]][daviddm-image] [![Coverage Status][coveralls-image]][coveralls-url]
+
+Julián Andrés López Valencia
+
+
+## Install
+
+```bash
+$ npm install --save veggie-node
+```
+
+
+## Usage
+
+```javascript
+var veggie = require('veggie-node');
+veggie(); // "awesome"
+```
+
+## API
+
+_(Coming soon)_
+
+
+## Contributing
+
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [gulp](http://gulpjs.com/).
+
+
+## License
+
+Copyright (c) 2016 Julián Andrés López Valencia. Licensed under the MIT license.
+
+
+
+[npm-url]: https://npmjs.org/package/veggie-node
+[npm-image]: https://badge.fury.io/js/veggie-node.svg
+[travis-url]: https://travis-ci.org/julianlopezv/veggie-node
+[travis-image]: https://travis-ci.org/julianlopezv/veggie-node.svg?branch=master
+[daviddm-url]: https://david-dm.org/julianlopezv/veggie-node.svg?theme=shields.io
+[daviddm-image]: https://david-dm.org/julianlopezv/veggie-node
+[coveralls-url]: https://coveralls.io/r/julianlopezv/veggie-node
+[coveralls-image]: https://coveralls.io/repos/julianlopezv/veggie-node/badge.png
---
+++ test/veggie_test.js
@@ -0,0 +1,48 @@
+/*
+ * Veggie Check!
+ * https://github.com/julianlopezv/veggie-node
+ *
+ * Copyright (c) 2016 Julián Andrés López Valencia
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var veggie = require('../');
+var assert = require('should');
+var request = require('request');
+var expect = require('chai').expect;
+
+describe('veggie', function () {
+
+	it('Verificar que la conexión se construye con los datos suministrados', function () {
+		veggie.makeConnection().should.not.equal(null);
+	});
+
+	it('Se hay error al validar el usuario debe decir SERVER ERROR CODE 45', function () {
+		veggie.validateAuth('[]', '[]', 'test').should.equal("SERVER ERROR CODE 45");
+	});
+
+	it('Un usuario con password "test" obtiene el token "c4ca4238a0b923820dcc509a6f75849b"', function () {
+		var user = {id:1, password:"098f6bcd4621d373cade4e832627b4f6"};
+		veggie.validateAuth(null, [user], '098f6bcd4621d373cade4e832627b4f6').token.should.equal('c4ca4238a0b923820dcc509a6f75849b');
+	});
+
+	it('Un usuario no registrado no debe obtener acceso', function () {
+		veggie.validateAuth(null, '[]', '').should.equal('NO AUTHORIZED');
+	});
+
+	it('El hash MD5 para "test" es "098f6bcd4621d373cade4e832627b4f6"', function () {
+		veggie.makeMd5('test').should.equal("098f6bcd4621d373cade4e832627b4f6");
+	});
+
+
+  	it('Service Auth: El usuario "test" con password "test" debe obtener el token "c4ca4238a0b923820dcc509a6f75849b"', function (done) {
+  		request.get('http://localhost/auth?username=test&password=test', function (err, res, body){
+    		expect(res.statusCode).to.equal(200);
+    		expect(res.body).to.equal('{"token":"c4ca4238a0b923820dcc509a6f75849b"}');
+    		done();
+  		});
+	});
+
+});
